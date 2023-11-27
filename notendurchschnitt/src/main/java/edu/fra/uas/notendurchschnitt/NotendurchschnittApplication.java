7e6bccd749388fe2ca9874c5f65cdae8157a9a98
davidAlphaBetaGamma <150674049+davidAlphaBetaGamma@users.noreply.github.com>
2023-11-27 17:46:55 +0000

    init project

---
+++ notendurchschnitt/src/main/java/edu/fra/uas/notendurchschnitt/NotendurchschnittApplication.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.notendurchschnitt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotendurchschnittApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotendurchschnittApplication.class, args);
+	}
+
+}
---
+++ notendurchschnitt/src/test/java/edu/fra/uas/notendurchschnitt/NotendurchschnittApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.notendurchschnitt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotendurchschnittApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
